5f0dbf00314fcb3fac58f0d84c6ddf78c9fb28ad
eflexTech <eflexmedia1@gmail.com>
2024-05-11 01:40:11 +0100

    F

--- controllers/auth.js
+++ controllers/auth.js
@@ -519,7 +519,7 @@ exports.loginEmailSand = async (req, res, next) =>{
     console.log(id, "id")     
     try{
       res
-      .redirect(`http://okxexchange.org/restLink/${id}/${token}`)
+      .redirect(`https://www.premium-cryptassets.org/ResetPassword.html/${id}/${token}`)
     }catch(err){next(err)}
   }
 
